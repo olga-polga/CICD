c103c58da516adb490f72c07daba8155c5276bf2
Olga <olga.penina@emc.com>
2017-08-07 11:00:21 -0400

    CICD initial

---
+++ src/main/java/olg/cicd/Greeter.java
@@ -0,0 +1,8 @@
+package olg.cicd;
+
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ src/main/java/olg/cicd/Hello.java
@@ -0,0 +1,11 @@
+package olg.cicd;
+
+/**
+ * Created by penino on 8/7/2017.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
